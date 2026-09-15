4a13a240a0a35c969ed77ad0aa5fb4b9904e7b39
Chonlasit tunpiriyakul <tleggoptle@gmail.com>
2026-09-15 13:53:40 +0700

    myjs11-15

---
+++ myjs13.js
@@ -0,0 +1,26 @@
+function test01(x, y) {
+    console.log(x);
+    y();
+}
+
+function test02(a, b, c) {
+    let data = a + c(20);
+    console.log(data);
+    b(11, 22, 33);
+}
+
+// ------------------------------------
+
+test01(555, function () {
+    console.log("Wow wow wow");
+});
+
+test02(
+    100,
+    (x, y, z) => { // ใส่ => เพิ่มตรงนี้
+        console.log(x + y + z);
+    },
+    function (m) {
+        return m * 5;
+    }
+);
